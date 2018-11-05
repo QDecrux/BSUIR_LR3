821ea51a98b954587e018e8b6dbd40da195ab736
Uladzimir Marchanka <marchenkofromminsk@gmail.com>
2018-11-05 09:49:26 +0300

    Fix auto in tree.h

--- 2/tree.h
+++ 2/tree.h
@@ -84,7 +84,7 @@ namespace tree
 			return Find(root_, key);
 		}
 		template<typename Ptr>
-		static auto& Find(Ptr& root, int key)
+		static Ptr& Find(Ptr& root, int key)
 		{
 			if (root.Empty()) return root;
 			if (root->data.first == key) return root;
